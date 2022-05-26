4ba0a401d3d57d717f7149a93b9fd3cf03b037e3
Simon Hoeferlin <s@hoeferlins.de>
2022-05-26 16:20:14 +0200

    Added linter

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+import express from 'express';
 
 const app = express();
 
@@ -6,7 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World! Und Servus Franz!');
 });
 
-let port = process.env.PORT || 8080
-app.listen(port, () =>
-  console.log('Example app listening on port 8080!'),
-);
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log('Example app listening on port 8080!'));
+
