af742a1a9476f9a2f5a6fde53e727c994cad1b6f
Simon Hoeferlin <s@hoeferlins.de>
2022-05-27 12:24:11 +0200

    Added .env setup

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,20 @@
+// Imports
 import express from 'express';
+import dotenv from 'dotenv';
 
+// Constants
+dotenv.config();
+const ENV = process.env;
+const DEFAULT_PORT = 8080;
+
+// Setup app
 const app = express();
 
+// Setup port (get from .env file or use default 8080)
+const port = ENV.PORT || DEFAULT_PORT;
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
+
+// Dummy respomse
 app.get('/', (req, res) => {
-  res.send('Hello World! Und Servus Franz!');
+  res.send(`Hello World! Und Servus ${ENV.ADVISOR}!`);
 });
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log('Example app listening on port 8080!'));
-
