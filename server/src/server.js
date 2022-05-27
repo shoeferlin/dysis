add45e230ea13e018cd5bdf3bc144a57b50a0b92
Simon Hoeferlin <s@hoeferlins.de>
2022-05-27 16:21:26 +0200

    Added logger

--- server/src/server.js
+++ server/src/server.js
@@ -2,33 +2,52 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import npmlog from 'npmlog';
 
 // Constants
 dotenv.config();
 const ENV = process.env;
 const DEFAULT_PORT = 8080;
 
+// Setup logger
+const log = npmlog;
+log.enableColor();
+
+// Create debug level (use 'log.debug(Prefix, Msg)' instead of 'console.log()')
+log.addLevel(
+    'debug',
+    10,
+    {bg: 'yellow', fg: 'white', bold: true, bell: true},
+    ' DEBUG ',
+);
+log.level = 'debug';
+
 // Setup app
 const app = express();
-console.log('Started server ...');
+log.info('SERVER START');
+log.info('SERVER START', 'Started server ...');
 
 // Connect database
 try {
   await mongoose.connect(ENV.MONGODB_URI).then(() => {
-    console.log('Database is connecting ...');
+    log.info('SERVER START', 'Database is connecting ...');
   });
 } catch (err) {
-  console.error(err);
+  log.error(err);
 }
+
 
 // Listen for error events on database
 mongoose.connection.on('error', (err) => {
-  console.error(err);
+  log.error(err);
 });
 
 // Setup port (get from .env or use default 8080)
 const port = ENV.PORT || DEFAULT_PORT;
-app.listen(port, () => console.log(`Server is listening on port ${port} ...`));
+app.listen(port, () => {
+  log.info('SERVER START', `Server is listening on port ${port} ...`);
+});
+
 
 // Dummy respomse
 app.get('/', (req, res) => {
