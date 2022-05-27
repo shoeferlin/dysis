1f33bd0a6faa3363bfdb340cad84f68eaf9b0b4f
Simon Hoeferlin <s@hoeferlins.de>
2022-05-27 15:12:21 +0200

    Added mongoDB connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 // Imports
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 // Constants
 dotenv.config();
@@ -9,10 +10,25 @@ const DEFAULT_PORT = 8080;
 
 // Setup app
 const app = express();
+console.log('Started server ...');
 
-// Setup port (get from .env file or use default 8080)
+// Connect database
+try {
+  await mongoose.connect(ENV.MONGODB_URI).then(() => {
+    console.log('Database is connecting ...');
+  });
+} catch (err) {
+  console.error(err);
+}
+
+// Listen for error events on database
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
+// Setup port (get from .env or use default 8080)
 const port = ENV.PORT || DEFAULT_PORT;
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+app.listen(port, () => console.log(`Server is listening on port ${port} ...`));
 
 // Dummy respomse
 app.get('/', (req, res) => {
