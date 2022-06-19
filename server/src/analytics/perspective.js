958384029a4ff7322eabd1d4198baffbd7c70e3a
Simon Hoeferlin <s@hoeferlins.de>
2022-06-19 12:46:40 +0200

    Using Perspective API client library

--- server/src/analytics/perspective.js
+++ server/src/analytics/perspective.js
@@ -1,4 +1,4 @@
-import {google} from 'googleapis';
+import Perspective from 'perspective-api-client';
 import {body} from 'express-validator';
 import dotenv from 'dotenv';
 
@@ -11,51 +11,35 @@ dotenv.config();
 const ENV = process.env;
 
 const API_KEY = ENV.GOOGLE_API_KEY;
-const DISCOVERY_URL = 'https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1';
+
+
+const perspective = new Perspective({
+  apiKey: API_KEY,
+});
 
 /**
- * Call Perspective API
- * Code taken from documentation page
- * @param {String} text
+ * Text
+ * @param {String} message
+ * @return {Result}
  */
-async function perspective(text) {
-  log.info(
-      'PERSPECTIVE API',
-      `Requesting perspective API for the following text:\n${text}`,
-  );
-  google.discoverAPI(DISCOVERY_URL)
-      .then((client) => {
-        const analyzeRequest = {
-          comment: {
-            text: text,
-          },
-          // Add more if necessary
-          requestedAttributes: {
-            TOXICITY: {},
-            INSULT: {},
-          },
-        };
-
-        client.comments.analyze(
-            {
-              key: API_KEY,
-              resource: analyzeRequest,
-            },
-            (err, response) => {
-              if (err) throw err;
-              log.info(
-                  'PERSPECTIVE API',
-                  JSON.stringify(response.data, null, 2),
-              );
-              return response.data;
-            },
-        );
-      })
-      .catch((err) => {
-        throw err;
-      });
+async function analyzeComment(message) {
+  try {
+    const result = await perspective.analyze({
+      'comment': {
+        'text': message,
+      },
+      'requestedAttributes': {
+        'TOXICITY': {},
+        'INSULT': {},
+        'SPAM': {},
+      },
+    });
+    return result;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
 }
-
 
 /**
  * Controller for handling incoming requests to test the Perspective AP
@@ -76,7 +60,7 @@ class PerspectiveController {
     // Actual controller method handling valid request
     async (req, res) => {
       const text = req.body.text;
-      const analysis = await perspective(text);
+      const analysis = await analyzeComment(text);
       try {
         const data = {
           perspective: analysis,
