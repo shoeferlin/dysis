a76ef324b6553d43ef39d52f5a36661058165d1c
Simon Hoeferlin <s@hoeferlins.de>
2022-08-11 12:33:49 +0200

    Increase sync from 10 to 5 mins

--- client/src/DysisConfig.ts
+++ client/src/DysisConfig.ts
@@ -9,7 +9,7 @@ export const dysisConfig = {
     defaultTrackingIntervalInSeconds: 1, 
   },
   sync: {
-    defaultSyncIntervalInMinutes: DEBUG ? 1 / 6 : 10,
+    defaultSyncIntervalInMinutes: DEBUG ? 1 / 6 : 5,
   },
   requests: {
     lowerBoundForFailedRequestTimeoutInSeconds: 5,
